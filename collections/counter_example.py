617e77a587e3f7b2847b27c2c5629fbe17ccaec3
Maheswari <maheswarim.work@gmail.com>
2025-04-23 21:09:09 -0400

    Counter collections

--- collections/counter_example.py
+++ collections/counter_example.py
@@ -1,9 +1,10 @@
 from collections import Counter
 
 def top_three_letters(input_string):
- print(Counter(input_string))
  return Counter(input_string).most_common(3)
 
 if __name__ == '__main__':
     input_string = "abbccabc"
     print(top_three_letters(input_string))
+    # Result
+    # [('b', 3), ('c', 3), ('a', 2)]
